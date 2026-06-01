1c3d633ba6de8632c5d37fbd39fe0a5153fbc60e
YMDprogrammer <sololarry268@gmail.com>
2026-05-31 20:21:49 -0400

    Configure Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jerfrim.appmobile',
+  appName: 'app-mobile-hexagonal',
+  webDir: 'www'
+};
+
+export default config;
